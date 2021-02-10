10fc1f4bea4bad98ebe4235d0de42acf63f33557
tomlishan <tomlishan>
2021-02-10 21:03:58 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
+    <SideBar />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SideBar from './components/SideBar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    SideBar
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <b-button v-b-toggle.sidebar-backdrop>Toggle Sidebar</b-button>
+
+    <b-sidebar
+      id="sidebar-backdrop"
+      title="Sidebar with backdrop"
+      :backdrop-variant="variant"
+      backdrop
+      shadow
+      bg-variant="dark"
+    >
+      <div class="px-3 py-2">
+        <b-form-group label="Backdrop variant" label-for="backdrop-variant">
+          
+          
+          <b-button variant="primary" block>button A</b-button>
+          <b-button variant="primary" block>button B</b-button>
+        </b-form-group>
+        
+      </div>
+
+    <template #footer>
+       <div class="d-flex bg-dark text-light align-items-center px-3 py-2">
+        <strong class="mr-auto">FooterA</strong>
+       </div>
+        <div class="d-flex bg-dark text-light align-items-center px-3 py-2">
+        <strong class="mr-auto">FooterB</strong>
+       </div>
+      </template>
+
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        variant: 'dark',
+        
+      }
+    }
+  }
+</script>
